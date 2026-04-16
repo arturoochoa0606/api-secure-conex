47c006b22954f6f7ed11ad02abaccb4d18a9294c
🪶Arturo  ochoa 🦅 Orange 🍊 <arturoochoa0606@gmail.com>
2026-04-16 11:35:14 -0600

    models.py

---
+++ models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+
+class Product(Base):
+    __tablename__ = "products"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    price = Column(Float)
+    stock = Column(Integer)
+
+class Order(Base):
+    __tablename__ = "orders"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    product_id = Column(Integer, ForeignKey("products.id"))
+    quantity = Column(Integer)
+
+    user = relationship("User")
+    product = relationship("Product")
